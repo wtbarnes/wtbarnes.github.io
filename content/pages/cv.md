2923529f08e3fc53e3b4b52b27601e98b34d9040
Will Barnes <wtb2@rice.edu>
2016-05-15 23:33:01 -0500

    rebuilding from scratch, stripping down to base theme

--- content/pages/cv.md
+++
@@ -1,6 +0,0 @@
----
-Title: CV
----
-<div class="lead">
-Oops! My CV didn't load. You can check it out on <a href="https://github.com/wtbarnes/resume">GitHub</a> if you'd like.
-</div>
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,10 +2,6 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-THEME='themes/pelican-themes/pelican-bootstrap3'
-BOOTSTRAP_THEME='readable'
-PYGMENTS_STYLE='emacs'
-
 AUTHOR = u'Will Barnes'
 SITENAME = u'Will Barnes'
 SITEURL = ''
@@ -16,8 +12,6 @@ TIMEZONE = 'America/Chicago'
 
 DEFAULT_LANG = u'en'
 
-DISPLAY_CATEGORIES_ON_MENU=False
-
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -25,49 +19,17 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-#path config
-STATIC=['images','notebooks']
-STATIC_PATHS=['images','notebooks','files']
-ARTICLE_EXCLUDES=['notebooks']
-
-#markup options
-MARKUP=('md','ipynb')
-
-#plugins
-PLUGIN_PATHS=['pelican-plugins','plugins']
-PLUGINS=['liquid_tags.notebook','ipynb.liquid',]#'ipynb.markdown']
-
 # Blogroll
-#LINKS = (('Pelican', 'http://getpelican.com/'),
-#         ('Python.org', 'http://python.org/'),
-#         ('Jinja2', 'http://jinja.pocoo.org/'),
-#         ('You can modify those links in your config file', '#'),)
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('twitter', 'https://twitter.com/wtbarnes_'),
-          ('github', 'https://github.com/wtbarnes'),
-          ('stackoverflow', 'http://stackoverflow.com/users/4717663/will-barnes', 'stack-overflow'))
-
-# About Me blurb
-AVATAR='/images/about_me.png'
-ABOUT_ME='PhD student at Rice University studying plasma processes in million-degree solar coronal loops.'
-
-# Github
-GITHUB_USER='wtbarnes'
-GITHUB_REPO_COUNT=3
-GITHUB_SKIP_FORK=False
-#GITHUB_SHOW_USER_LINK=False
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
-#banner
-BANNER='images/sdo_aia_171_loops_banner.png'
-
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-
-#Article and page pretty urls
-ARTICLE_URL = '{slug}/'
-ARTICLE_SAVE_AS = '{slug}/index.html'
-PAGE_URL = '{slug}/'
-PAGE_SAVE_AS = '{slug}/index.html'
