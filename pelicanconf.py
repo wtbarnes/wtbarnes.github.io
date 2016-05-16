26bfcce71634fb6b4c2975af84812f76a28c3ec9
Will Barnes <wtb2@rice.edu>
2016-05-16 00:09:54 -0500

    adding bootstrap 3 theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,6 +2,12 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+THEME='themes/pelican-themes/pelican-bootstrap3'
+BOOTSTRAP_THEME='readable'
+PYGMENTS_STYLE='emacs'
+
+DISPLAY_CATEGORIES_ON_MENU=False
+
 AUTHOR = u'Will Barnes'
 SITENAME = u'Will Barnes'
 SITEURL = ''
@@ -20,16 +26,29 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'https://twitter.com/wtbarnes_'),
+          ('github', 'https://github.com/wtbarnes'),
+          ('stackoverflow', 'http://stackoverflow.com/users/4717663/will-barnes', 'stack-overflow'))
+
+# Github
+GITHUB_USER='wtbarnes'
+GITHUB_REPO_COUNT=3
+GITHUB_SKIP_FORK=False
+#GITHUB_SHOW_USER_LINK=False
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+#Article and page pretty urls
+ARTICLE_URL = '{slug}/'
+ARTICLE_SAVE_AS = '{slug}/index.html'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
