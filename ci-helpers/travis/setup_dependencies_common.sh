21377cb7408a5cc8eaf3615f7df5c8cdab97e254
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-01-19 17:29:39 +0000

    Update docs after building Travis build 158 of wtbarnes/wtbarnes.github.io
    The docs were built from the branch 'sources' against the commit
    2e34afda4e5c33616142680b7fa80d877920da70.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/wtbarnes/wtbarnes.github.io/jobs/481807544.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test/bin/doctr deploy . --built-docs www/ --deploy-branch-name master --no-require-master

--- ci-helpers/travis/setup_dependencies_common.sh
+++ ci-helpers/travis/setup_dependencies_common.sh
@@ -135,7 +135,7 @@ fi
 # We pin the version for conda as it's not the most stable package from
 # release to release. Add note here if version is pinned due to a bug upstream.
 if [[ -z $CONDA_VERSION ]]; then
-    CONDA_VERSION=4.5.10
+    CONDA_VERSION=4.5.12
 fi
 
 if [[ -z $PIN_FILE_CONDA ]]; then
@@ -472,13 +472,6 @@ if [[ $SETUP_CMD == *build_sphinx* ]] || [[ $SETUP_CMD == *build_docs* ]]; then
     # account
     if [[ -z $SIP_VERSION ]]; then
         echo "sip <4.19" >> $PIN_FILE
-    fi
-
-    # Temporary version limitation until
-    # https://github.com/sphinx-doc/sphinx/issues/4689 (affecting 1.7) is
-    # addressed and a new version of sphinx is released.
-    if [[ -z $SPHINX_VERSION ]]; then
-        SPHINX_VERSION='<1.7'
     fi
 
     if [[ ! -z $SPHINX_VERSION ]]; then
